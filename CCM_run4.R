962d3c186b693039f1b8da183d71cc648c08a2df
Jonathan Rodemann <rodemann@fleabag.local>
2025-10-27 09:38:08 -0700

    update on CCMs

--- CCM_run4.R
+++ CCM_run4.R
@@ -36,7 +36,7 @@ names(dat)
 
 # Select variables for CCM test
 y <- "rchl"  # effect
-x <- "chp_stage"  # cause
+x <- "cTP"  # cause
 df1 <- dat[,c("date",x,y)] |> na.omit()
 df1$date <- df1$date |> ymd() # format dates
 df1[,c(2,3)] <- apply( df1[,c(2,3)], 2, scale )  # scale signals to mean=0, sd=1
@@ -67,7 +67,7 @@ ccm$CCM1_PredictStat |> tail()
 #png("E:/FIU/PostDoc/FB_sediment_algal_blooms/Project/Data/Figures_EDM/area_gsmeanstage.png", width = 800, height = 600, res = 100)
 plot( x = ccm$LibMeans$LibSize,
       y = ccm$LibMeans[,2],
-      main = 'Rankin Chlorophyll xmap Marsh Stage',
+      main = 'Rankin Chlorophyll xmap Gulf Total Phosphorus',
       ylab = "Prediction skill", xlab = "Library size",
       ylim = range( 0, range(ccm$LibMeans[,2]), 1 ),
       type = 'l', col = 1, lwd = 1 )
