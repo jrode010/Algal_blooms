1f62e4d0c295df5f569886f3a964f71084559df6
jrode010 <jrode010@fiu.edu>
2025-10-30 18:54:26 -0700

    time series of stuff

--- CCM_run4.R
+++ CCM_run4.R
@@ -34,9 +34,13 @@ dat <- cbind(dat, datjg)
 dat <- cbind(dat, datchpstage)
 names(dat)
 
+ggplot(dat)+
+  geom_line(aes(x = ymd(date), y = gTP))+
+  theme_classic()
+
 # Select variables for CCM test
 y <- "rchl"  # effect
-x <- "cTP"  # cause
+x <- "chp_stage"  # cause
 df1 <- dat[,c("date",x,y)] |> na.omit()
 df1$date <- df1$date |> ymd() # format dates
 df1[,c(2,3)] <- apply( df1[,c(2,3)], 2, scale )  # scale signals to mean=0, sd=1
@@ -67,7 +71,7 @@ ccm$CCM1_PredictStat |> tail()
 #png("E:/FIU/PostDoc/FB_sediment_algal_blooms/Project/Data/Figures_EDM/area_gsmeanstage.png", width = 800, height = 600, res = 100)
 plot( x = ccm$LibMeans$LibSize,
       y = ccm$LibMeans[,2],
-      main = 'Rankin Chlorophyll xmap Gulf Total Phosphorus',
+      main = 'Rankin Chlorophyll xmap Marsh Stage',
       ylab = "Prediction skill", xlab = "Library size",
       ylim = range( 0, range(ccm$LibMeans[,2]), 1 ),
       type = 'l', col = 1, lwd = 1 )
@@ -928,16 +932,6 @@ gdcbrerchl <- graphlag(dcbrerchl, rhorchldcbre)
 gdcbrerchl
 gcDOjchl <- graphlag(cDOjchl, rhojchlcDO)
 gcDOjchl
-getnjchl <- graphlag(eTNjchl, rhojchleTN)
-getnjchl
-getocjchl <- graphlag(eTOCjchl, rhojchleTOC)
-getocjchl
-gjturbjchl <- graphlag(jturbjchl, rhojchljturb)
-gjturbjchl
-gactpgph <- graphlag(actpgpH, rhogpHactp)
-gactpgph
-ggnh4gph <- graphlag(gnh4gpH, rhogpHgnh4)
-ggnh4gph
 
 ggsave(filename = 'plots/gtocarea_lag.png', plot = ggtocarea)
 ggsave(filename = 'plots/mcdocarea_lag.png', plot = gmcdocarea)
@@ -977,10 +971,6 @@ ggsave(filename = 'plots/rchleturb_lag.png', plot = geturbrchl)
 ggsave(filename = 'plots/rchletn_lag.png', plot = getnrchl)
 ggsave(filename = 'plots/rchldcbre_lag.png', plot = gdcbrerchl)
 ggsave(filename = 'plots/areadcbre_lag.png', plot = gdcbrearea)
-ggsave(filename = 'plots/jchletn_lag.png', plot = getnjchl)
-ggsave(filename = 'plots/jchljturb_lag.png', plot = gjturbjchl)
-ggsave(filename = 'plots/gphactp_lag.png', plot = gactpgph)
-ggsave(filename = 'plots/gphgn4_lag.png', plot = ggnh4gph)
 
 ##CCM graphs at the max lag for significant variables
 #function to graph
